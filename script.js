09852c23cae4dbca992a8d88655c6fb0df5c6c89
Blobified <87593231+Zycostan@users.noreply.github.com>
2026-06-05 08:56:58 +0000

    dirty, ewww

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ const handleOnMove = e => {
     left.style.width = `${p}%`;
 }
 
+const handleOnMouseMove = e => {
+   const {  currentTarget: target } = e;
+
+   const rect = target.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+   target.style.setProperty("--mouse-x",`${x}px`)
+   target.style.setProperty("--mouse-y",`${y}px`)
+}
+
 header.onmousemove = e => handleOnMove(e);
 
 header.ontouchmove = e => handleOnMove(e.touches[0]);
+
+for(const card of document.querySelectorAll(".card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+}
